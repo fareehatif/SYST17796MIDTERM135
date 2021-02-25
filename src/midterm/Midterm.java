d3f5b040da7c87a83066a008cf824cefc92847c6
fareehatif <mirzfare@sheridancollege.ca>
2021-02-25 12:58:13 -0500

    Fareeha Mirza 991489251

---
+++ src/midterm/Midterm.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Midterm;
+
+/**
+ *
+ * @author r2kar
+ */
+
+public class Midterm {
+
+    /**
+     * @param args the command line arguments
+     */
+    Seasons season;
+    private boolean abc=true;
+    public enum Seasons{
+        FALL,
+        WINTER,
+        SPRING,
+        SUMMER
+    }
+    //a constructor
+    public Midterm(Seasons season)
+    {
+        this.season = season;
+    }
+   
+    public void seasonDescription()
+    {
+       switch (season){
+            case FALL:
+                System.out.println("my fav. season!");
+                break;
+            case WINTER:
+                System.out.println("it is too cold!");
+                break;
+            case SPRING:
+                System.out.println("my allerg!");
+                break;
+            case SUMMER:
+                System.out.println("it is hot!");
+                break;
+     }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // try to get the input (myStr) from the user
+        String myStr= "SUMMER";
+        Midterm test1= new Midterm(Seasons.valueOf(myStr));
+        test1.seasonDescription();
+        //test1.isSeason();
+        int ord = Seasons.valueOf(myStr).ordinal();
+        System.out.println(Seasons.valueOf(myStr).ordinal());
+        //a for loop that iterated thru the Seasons Enum using values() method
+        for (Seasons mySeason: Seasons.values())
+        {
+            
+            System.out.println(mySeason);
+        }
+       
+        }
+    }
+    
